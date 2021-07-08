0f34121b8e0b5377354eb539ea3ac03839019929
Mikhail Ponomarev <michailp800@gmail.com>
2021-07-08 13:33:48 +0300

    v1

---
+++ src/main/java/ru/netology/domain/PostPublicationDate.java
@@ -0,0 +1,22 @@
+package ru.netology.domain;
+
+public class PostPublicationDate {
+    public String currentDate;
+    public String currentTime;
+
+    public String getCurrentDate() {
+        return currentDate;
+    }
+
+    public void setCurrentDate(String currentDate) {
+        this.currentDate = currentDate;
+    }
+
+    public String getCurrentTime() {
+        return currentTime;
+    }
+
+    public void setCurrentTime(String currentTime) {
+        this.currentTime = currentTime;
+    }
+}
---
+++ src/main/java/ru/netology/domain/Repost.java
@@ -0,0 +1,14 @@
+package ru.netology.domain;
+
+public class Repost {
+    private int[] friendsList;
+
+    public int[] getFriendsList() {
+        return friendsList;
+    }
+
+    public void setFriendsList(int[] friendsList) {
+        this.friendsList = friendsList;
+    }
+
+}
